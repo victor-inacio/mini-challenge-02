5a1cd6baa92e4eaf640e9f1136834855d194c106
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-09-21 18:01:17 -0300

    Statistics
    Added some UI elements to StatistcsView

--- Mini02/Mini02/ViewControllers/Statistic/StatisticViewController.swift
+++ Mini02/Mini02/ViewControllers/Statistic/StatisticViewController.swift
@@ -11,7 +11,44 @@ class StatisticViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .cyan
+        self.view.backgroundColor = .white
+        
+        // Cria o primeiro título
+        let titleLabel1 = UILabel()
+        titleLabel1.text = "Estatísticas"
+        titleLabel1.font = UIFont.boldSystemFont(ofSize: 40)
+        titleLabel1.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(titleLabel1)
+        
+        // Cria o segundo título
+        let titleLabel2 = UILabel()
+        titleLabel2.text = "Informações Gerais"
+        titleLabel2.font = UIFont.systemFont(ofSize: 18)
+        titleLabel2.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(titleLabel2)
+        
+        // Cria uma instância de StatisticsBoxView para exibir as estatísticas
+        let statisticsBox = StatisticsBoxView()
+        statisticsBox.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(statisticsBox)
+        
+        // Configura as constraints para posicionar os títulos e a box de estatísticas
+        NSLayoutConstraint.activate([
+            // Constraints para o primeiro título (Título Maior)
+            titleLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            
+            // Constraints para o segundo título (Informações Gerais)
+            titleLabel2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel2.topAnchor.constraint(equalTo: titleLabel1.bottomAnchor, constant: 10),
+            
+            // Constraints para a box de estatísticas
+            statisticsBox.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            statisticsBox.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            statisticsBox.topAnchor.constraint(equalTo: titleLabel2.bottomAnchor, constant: 10),
+        ])
     }
-
 }
--- Mini02/Mini02/ViewControllers/TabView/TabBarController.swift
+++ Mini02/Mini02/ViewControllers/TabView/TabBarController.swift
@@ -17,19 +17,19 @@ class TabBarController: UITabBarController{
     
     func setup(){
         let vc1 = HomeViewController()
-        let vc2 = StatisticViewController()
-        let vc3 = JournalViewController()
+        let vc2 = JournalViewController()
+        let vc3 = StatisticViewController()
         
         vc1.tabBarItem.title = "Home"
-        vc1.tabBarItem.image = UIImage(systemName: "gear")
+        vc1.tabBarItem.image = UIImage(systemName: "house")
         let nav1 = UINavigationController(rootViewController: vc1)
         
         vc2.tabBarItem.title = "Journal"
-        vc2.tabBarItem.image = UIImage(systemName: "pencil")
+        vc2.tabBarItem.image = UIImage(systemName: "book.fill")
         let nav2 = UINavigationController(rootViewController: vc2)
         
         vc3.tabBarItem.title = "Statistic"
-        vc3.tabBarItem.image = UIImage(systemName: "star.fill")
+        vc3.tabBarItem.image = UIImage(systemName: "chart.bar")
         let nav3 = UINavigationController(rootViewController: vc3)
         
         tabBar.backgroundColor = .systemGray
