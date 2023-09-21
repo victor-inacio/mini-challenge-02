1f1e5761017588ce91176f57138c6bfb6a8ccc41
Victor Inácio <chustrupgamer@gmail.com>
2023-09-21 17:14:19 -0300

    PersistentContainer implemented on AppDelegate

--- Mini02/Mini02/Support/AppDelegate.swift
+++ Mini02/Mini02/Support/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    
+    var dataController: DataController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        dataController = DataController()
+        
+        
+        
         return true
     }
 
