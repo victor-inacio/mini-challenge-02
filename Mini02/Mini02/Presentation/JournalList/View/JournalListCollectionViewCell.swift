c6c76b1fb67400f3d1523b065800f8a9fcfab032
Victor Inácio <chustrupgamer@gmail.com>
2023-09-27 15:56:01 -0300

    Separated in files

---
+++ Mini02/Mini02/Presentation/JournalList/View/JournalListCollectionViewCell.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class JournalListCollectionViewCell: UICollectionViewCell {
+    
+    static var CellIdentifier = "CustomCell"
+    let background = UIView()
+    let stackView = {
+        let stackView = StackView(axis: .horizontal, spacing: 10)
+        stackView.backgroundColor = .red
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layer.cornerRadius = 16
+        stackView.layoutMargins = .init(top: 0, left: 20, bottom: 0, right: 20)
+        return stackView
+    }()
+    let date = Label(text: "11/11")
+    let title = Label(text: "Title")
+    let iconImage = UIImageView(image: .init(named: "feeling_1"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+        stackView.addArrangedSubview(date)
+        
+        stackView.addArrangedSubview(title)
+        stackView.addArrangedSubview(iconImage)
+        
+        
+        contentView.addSubview(stackView)
+        
+        background.backgroundColor = .red
+        background.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(background)
+        
+        NSLayoutConstraint.activate([
+            
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    
+}
+
+#Preview {
+    JournalListCollectionViewCell()
+}
