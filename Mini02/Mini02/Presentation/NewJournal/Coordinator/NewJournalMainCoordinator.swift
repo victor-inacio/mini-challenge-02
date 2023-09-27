1a9e8b37076c9c3a084a3dda396bd4212ea74a62
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2023-09-27 18:11:13 -0300

    TextField e TextView, adicionadas na tela de New Journal.

---
+++ Mini02/Mini02/Presentation/NewJournal/Coordinator/NewJournalMainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  NewJournalMainCordinator.swift
+//  Mini02
+//
+//  Created by Thiago Pereira de Menezes on 26/09/23.
+//
+
+import Foundation
---
+++ Mini02/Mini02/Presentation/NewJournal/ViewModel/NewJournalViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewJournalViewModel.swift
+//  Mini02
+//
+//  Created by Thiago Pereira de Menezes on 26/09/23.
+//
+
+import Foundation
