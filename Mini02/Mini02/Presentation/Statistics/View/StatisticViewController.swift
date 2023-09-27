3e4263314265bfc53bd968f12462339d5e14731e
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-09-27 17:50:21 -0300

    Scroll StatisticViewController
    Scroll adicionado a StatisticViewController

--- Mini02/Mini02/Presentation/Statistics/View/StatisticViewController.swift
+++ Mini02/Mini02/Presentation/Statistics/View/StatisticViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Luca Lacerda on 21/09/23.
 //
-
 import UIKit
 
 class StatisticViewController: UIViewController, MVVMCView {
@@ -15,68 +14,114 @@ class StatisticViewController: UIViewController, MVVMCView {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         
+        // MARK: - Configuração do UIScrollView
+        
+        // Cria um UIScrollView para conter todos os elementos
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scrollView)
+        
+        // MARK: - Configuração do contentView
+        
+        // Cria uma view para colocar todos os elementos dentro do UIScrollView
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        
+        // MARK: - Configuração dos títulos
+        
         // Cria o primeiro título
-        let titleLabel1 = Label(text: "Estatísticas")
+        let titleLabel1 = UILabel()
+        titleLabel1.text = "Estatísticas"
         titleLabel1.font = UIFont.boldSystemFont(ofSize: 40)
+        titleLabel1.translatesAutoresizingMaskIntoConstraints = false
         
         // Cria o segundo título
-        let titleLabel2 = Label(text: "Informações Gerais")
+        let titleLabel2 = UILabel()
+        titleLabel2.text = "Informações Gerais"
         titleLabel2.font = UIFont.boldSystemFont(ofSize: 18)
+        titleLabel2.translatesAutoresizingMaskIntoConstraints = false
         
         // Cria o terceiro título
-        let titleLabel3 = Label(text: "Sentimentos")
+        let titleLabel3 = UILabel()
+        titleLabel3.text = "Sentimentos"
         titleLabel3.font = UIFont.boldSystemFont(ofSize: 18)
+        titleLabel3.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(titleLabel1)
-        self.view.addSubview(titleLabel2)
-        self.view.addSubview(titleLabel3)
+        contentView.addSubview(titleLabel1)
+        contentView.addSubview(titleLabel2)
+        contentView.addSubview(titleLabel3)
+        
+        // MARK: - Configuração de espaçamento
         
         let spacing: CGFloat = 20
+        
+        // MARK: - Configuração da box
         
         let boxView = UIView()
         boxView.backgroundColor = UIColor(named: "box_bg")
         boxView.layer.cornerRadius = 15
         boxView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(boxView)
         
-        self.view.addSubview(boxView)
+        // MARK: - Configuração de StatisticsBoxView
         
         // Cria uma instância de StatisticsBoxView para exibir as estatísticas
         let statisticsBox = StatisticsBoxView()
         statisticsBox.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(statisticsBox)
         
-        self.view.addSubview(statisticsBox)
+        // MARK: - Configuração de Constraints
         
-        // Configura as constraints para posicionar os títulos e a box de estatísticas
+        // Configura as constraints para posicionar os títulos e a box de estatísticas dentro do contentView
         NSLayoutConstraint.activate([
             // Constraints para o primeiro título (Título Maior)
-            titleLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        //  titleLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            titleLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            titleLabel1.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20), // Leva em consideração a safe area
+            titleLabel1.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20), // Margem à direita
+            
             // Constraints para o segundo título (Informações Gerais)
-            titleLabel2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel2.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             titleLabel2.topAnchor.constraint(equalTo: titleLabel1.bottomAnchor, constant: 10),
+            titleLabel2.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             
             // Constraints para a box de estatísticas
-            statisticsBox.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            statisticsBox.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            statisticsBox.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            statisticsBox.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             statisticsBox.topAnchor.constraint(equalTo: titleLabel2.bottomAnchor, constant: 10),
             
             // Constraints para o terceiro título (Outras Informações)
-            titleLabel3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel3.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             titleLabel3.topAnchor.constraint(equalTo: statisticsBox.bottomAnchor, constant: spacing),
+            titleLabel3.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             
             // Constraints para a caixa de fundo
-            boxView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            boxView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            boxView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            boxView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             boxView.topAnchor.constraint(equalTo: titleLabel3.bottomAnchor, constant: 10),
             boxView.heightAnchor.constraint(equalToConstant: 100),
+            boxView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20), // Garante que o contentView seja maior que a boxView
+        ])
+        
+        // MARK: - Configuração de Constraints para o ScrollView e contentView
+        
+        // Configura as constraints para o scrollView e contentView
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor), // Garante que o contentView seja do mesmo tamanho que o scrollView
+            contentView.bottomAnchor.constraint(greaterThanOrEqualTo: scrollView.bottomAnchor), // Garante o conteúdo suficiente para rolagem
         ])
     }
 }
 
-
-  #Preview{
-      StatisticViewController()
-  }
-
+#Preview{
+    StatisticViewController()
+}
