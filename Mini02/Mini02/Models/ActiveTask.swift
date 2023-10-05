23e0ed07b4fa6656c32bd75cbb94abb8e7ef0ed6
Victor Inácio <chustrupgamer@gmail.com>
2023-10-05 15:47:55 -0300

    Created model for ActiveTask

---
+++ Mini02/Mini02/Models/ActiveTask.swift
@@ -0,0 +1,52 @@
+import CoreData
+
+extension ActiveTask {
+    
+    static func getUncompletedTasksByDate(date: Date) throws -> [ActiveTask] {
+        
+        let predicate = NSPredicate(format: "created_at <= %@ && completed_at == nil", date as NSDate)
+        
+        let dataController = DataController()
+        let context = dataController.viewContext
+        
+        let fetchRequest = fetchRequest()
+        fetchRequest.predicate = predicate
+        
+        let data = try context.fetch(fetchRequest)
+        
+        return data
+    }
+    
+    static func getCompletedTasksByDate(date: Date) throws -> [ActiveTask] {
+        let predicate = NSPredicate(format: "completed_at == %@", date as NSDate)
+        
+        let dataController = DataController()
+        let context = dataController.viewContext
+        
+        let fetchRequest = fetchRequest()	
+        fetchRequest.predicate = predicate
+        
+        let data = try context.fetch(fetchRequest)
+        
+        return data
+    }
+    
+    func complete(date: Date) throws {
+        let dataController = DataController()
+        let context = dataController.viewContext
+        
+        completed_at = date
+        
+        try dataController.save()
+    }
+    
+    func uncomplete(date: Date) throws {
+        let dataController = DataController()
+        let context = dataController.viewContext
+        
+        completed_at = nil
+        
+        try dataController.save()
+    }
+    
+}
