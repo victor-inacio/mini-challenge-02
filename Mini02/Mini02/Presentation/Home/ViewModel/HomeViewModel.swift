dc3be0c0a84e4ee988ba35ac242cc61a35454572
Victor Inácio <chustrupgamer@gmail.com>
2023-10-05 16:39:29 -0300

    Added functions to HomeViewModel

--- Mini02/Mini02/Presentation/Home/ViewModel/HomeViewModel.swift
+++ Mini02/Mini02/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-// ViewModel da Home
 class HomeViewModel: ViewModel {
     
     private var homeViewController: HomeViewController
@@ -19,6 +18,48 @@ class HomeViewModel: ViewModel {
     
     func dateString(date: Date) {
         datePickerDate = dateToString.makeDate(date: date)
+    }
+    
+    func handle(error: Error) {
+        print(error)
+    }
+    
+    func getUncompletedTasks(date: Date) -> [ActiveTask] {
+        do {
+            let tasks = try ActiveTask.getUncompletedTasksByDate(date: date)
+            return tasks
+        } catch {
+            handle(error: error)
+        }
+        
+        return []
+    }
+    
+    func getCompletedTasks(date: Date) -> [ActiveTask] {
+        do {
+            let tasks = try ActiveTask.getCompletedTasksByDate(date: date)
+            return tasks
+        } catch {
+            handle(error: error)
+        }
+        
+        return []
+    }
+    
+    func completeTask(task: ActiveTask, date: Date) {
+        do {
+            try task.complete(date: date)
+        } catch {
+            handle(error: error)
+        }
+    }
+    
+    func uncompleteTask(task: ActiveTask, date: Date) {
+        do {
+            try task.uncomplete(date: date)
+        } catch {
+            handle(error: error)
+        }
     }
     
 }
