91801c9e65b98d7e462f65e8563c73598a4a74f4
Victor Inácio <chustrupgamer@gmail.com>
2023-10-07 20:50:10 -0300

    Added getAll() static function o Journal Model

---
+++ Mini02/Mini02/Models/Journal.swift
@@ -0,0 +1,15 @@
+import CoreData
+
+extension Journal {
+    
+    static func getAll() throws -> [Journal] {
+        let fetchRequest = fetchRequest()
+        let dataController = DataController()
+        let context = dataController.viewContext
+        
+        let journals = try context.fetch(fetchRequest)
+        
+        return journals
+    }
+    
+}
