50e6053eb71c7bdce321a8381954bda9448f91fa
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2023-10-15 12:07:34 -0300

    altas alteracoes

--- Mini02/Mini02/Presentation/Home/ModalTips/ModalTips.swift
+++ Mini02/Mini02/Presentation/Home/ModalTips/ModalTips.swift
@@ -10,6 +10,7 @@ import UIKit
 class ModalTips: UIView {
     
     var isOpen = false
+    
     let buttonClose: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Voltar", for: .normal)
@@ -18,16 +19,39 @@ class ModalTips: UIView {
     
     let title: UILabel = {
         let label = UILabel()
+        label.textColor = UIColor.modalTipsTitle
+        label.font = UIFontMetrics.default.scaledFont(for: UIFont(name: "Nunito-Bold", size: 26)!)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         return label
+    }()
+    
+    let divTitle: UIView = {
+        let div = UIView()
+        div.backgroundColor = .clear
+        return div
     }()
     
     let titleLevel: UILabel = {
         let label = UILabel()
+        label.textColor = UIColor.modalTipsTitleTaskLevel
+        label.font = UIFontMetrics.default.scaledFont(for: UIFont(name: "Nunito-Bold", size: 20)!)
+        
+        // Create a new Attributed String
+        let attributedString = NSMutableAttributedString.init(string: "Text on label")
+
+        // Add Underline Style Attribute.
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range:
+            NSRange.init(location: 0, length: attributedString.length));
+        label.attributedText = attributedString
+
         return label
     }()
-    
+     
     let labelDescription: UILabel = {
         let label = UILabel()
+        label.textColor = UIColor.modalTipsTextDescription
+        label.font = UIFontMetrics.default.scaledFont(for: UIFont(name: "Nunito", size: 15)!)
         return label
     }()
     
@@ -40,12 +64,9 @@ class ModalTips: UIView {
     init() {
         super.init(frame: .zero)
         
-        backgroundColor = .systemPink
-        layer.cornerRadius = 40
+        backgroundColor = .modalTipsBackground
+        layer.cornerRadius = 30
         translatesAutoresizingMaskIntoConstraints = false
-        
-                
-       
         
         setup()
     }
@@ -56,10 +77,11 @@ class ModalTips: UIView {
     
     private func setup() {
         setupButtonClose()
-        setupTitle()
+        setupDivTitle()
         setupLevelTitle()
         setupDivisor()
         setupLabelDescription()
+        setupVStack()
     }
     
     private func setupButtonClose() {
@@ -71,26 +93,54 @@ class ModalTips: UIView {
         ])
     }
     
-    private func setupTitle() {
-        title.text = titleContent
-        
+    private func setupDivTitle() {
+        addSubview(divTitle)
+        divTitle.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            title.centerXAnchor.constraint(equalTo: centerXAnchor),
+            divTitle.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8)
         ])
-        
+        setupTitle()
     }
     
-    private func setupLevelTitle() {
-        titleLevel.text = titleLevelContent
+    private func setupTitle() {
+        title.text = titleContent
+        divTitle.addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.textAlignment = .center  // Alinhe o texto ao centro
+
+        // Adicione uma restrição de largura máxima
+        let maxWidthConstraint = title.widthAnchor.constraint(lessThanOrEqualTo: divTitle.widthAnchor)
+        maxWidthConstraint.priority = .defaultHigh
+        maxWidthConstraint.isActive = true
 
         NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: divTitle.centerXAnchor),
+            title.topAnchor.constraint(equalTo: buttonClose.bottomAnchor, constant: 8), // Ajuste o valor conforme necessário
+        ])
+    }
+
+    private func setupLevelTitle() {
+        titleLevel.text = titleLevelContent
+        titleLevel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLevel)
+        NSLayoutConstraint.activate([
             titleLevel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLevel.topAnchor.constraint(equalTo: title.bottomAnchor)
         ])
         
     }
     
     private func setupDivisor() {
         
+    }
+    
+    private func setupVStack() {
+        addSubview(VSTack)
+        VSTack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            VSTack.centerXAnchor.constraint(equalTo: centerXAnchor),
+            VSTack.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
     
     private func setupLabelDescription() {
